c0e11de0237808786cd53b86f180cb4b783bf8f4
Michael Ajala <agmt5989@gmail.com>
2019-05-02 09:23:06 +0100

    complete scheduler

--- sched.php
+++ sched.php
@@ -3,19 +3,27 @@ $servername = "us-cdbr-iron-east-02.cleardb.net";
 $dbname = "heroku_88ad8453f8bac06";
 $username = "bb982c65d583a5";
 $password = "8c39acfb";
-$myfile = fopen("file.txt", "w")  or die("Unable to open file!");
+// $myfile = fopen("file.txt", "w")  or die("Unable to open file!");
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $conn->prepare("SELECT id, imageUrl FROM articles");
-    $stmt.execute();
+    $stmt->execute();
     // set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
 
-    foreach ($stmt->fetchAll() as $key => $value) {
-    	fwrite($myfile, $value . "\n");
+    foreach ($stmt->fetchAll() as $v) {
+    	// var_dump($v);
+    	// echo ($v['id'] . "\t" . $v['imageUrl'] . "\r\n");
+    	if (substr($v['imageUrl'], 0, 5) == 'http:') {
+    	 	$nUrl = str_replace('http:', 'https:', $v['imageUrl']);
+    	 	// echo $nUrl;
+    	 	$q = $conn->prepare("UPDATE articles SET imageUrl='$nUrl' WHERE id=" . $v['id']);
+    	 	$q->execute();
+    	 } 
+    	// fwrite($myfile, $value . "\n");
     }
     $conn = null;
     }
@@ -23,6 +31,4 @@ catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-?>
-
 ?>
